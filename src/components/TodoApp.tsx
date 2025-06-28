f79227bf08c2aa507af2a05566478469a3ef7e1d
Tyron Landman <tyron.j.landman@gmail.com>
2025-06-29 08:41:31 +1200

    added custom hooks to clean up TodoApp.js

--- src/components/TodoApp.tsx
+++ src/components/TodoApp.tsx
@@ -1,59 +1,25 @@
-import React, { useEffect, useState } from "react";
-import type { Todo } from "../types/todo";
+import React, { useState } from "react";
 import TodoList from "./TodoList";
 import ClearCompleted from "./ClearCompleted";
 import TodoModal from "./TodoModal";
-import { getAllTasks, saveTaskstoStorage } from "../utils/localStorage";
-import debounce from "lodash/debounce";
 import FilterButtons from "./FilterButtons";
 import { Filters } from "../types/Filters";
+import { useTodos } from "../hooks/useTodos";
 
 const TodoApp: React.FC = () => {
-  const [todos, setTodos] = useState<Todo[]>(getAllTasks());
+  const { todos, addTodo, toggleTodo, deleteTodo, clearCompletedTodos } =
+    useTodos();
+
   const [isCreatingNewTask, setIsCreatingNewTask] = useState<boolean>(false);
   const [filter, setFilter] = useState<Filters>(Filters.ALL);
 
-  useEffect(() => {
-    const handler = debounce(() => {
-      saveTaskstoStorage(todos);
-    }, 300);
-    handler();
-    return () => {
-      handler.cancel();
-    };
-  }, [todos]);
-
-  function handleClearCompleted() {
-    setTodos(todos.filter((x) => !x.completed));
-  }
-
-  function removeTodo(id: string) {
-    setTodos((prevTodos) => prevTodos.filter((x) => x.id !== id));
-  }
-
-  function handleToggleComplete(id: string) {
-    setTodos((prevTodos) =>
-      prevTodos.map((todo) =>
-        todo.id === id ? { ...todo, completed: !todo.completed } : todo
-      )
-    );
+  function handleFilterChange(filter: Filters) {
+    setFilter(filter);
   }
 
   function createNewTask(newTask: string) {
-    const todo: Todo = {
-      id: crypto.randomUUID(),
-      text: newTask,
-      completed: false,
-      createdAt: Date.now(),
-    };
-
-    setTodos((prevTodos) => [...prevTodos, todo]);
-
+    addTodo(newTask);
     setIsCreatingNewTask(false);
-  }
-
-  function handleFilterChange(filter: Filters) {
-    setFilter(filter);
   }
 
   return (
@@ -74,11 +40,11 @@ const TodoApp: React.FC = () => {
           if (filter === Filters.COMPLETED) return todo.completed;
           if (filter === Filters.PENDING) return !todo.completed;
         })}
-        removeTodo={removeTodo}
-        handleToggleComplete={handleToggleComplete}
+        removeTodo={deleteTodo}
+        handleToggleComplete={toggleTodo}
       />
       <footer>
-        <ClearCompleted onPressClear={handleClearCompleted} />
+        <ClearCompleted onPressClear={clearCompletedTodos} />
       </footer>
     </div>
   );
